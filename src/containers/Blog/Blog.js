47b670fb0844bb74e9c8e484fcaf80a0ade64bb3
Keisuke Baba <kisk0419@gmail.com>
2021-03-21 12:13:36 +0900

    to #209

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 class Blog extends Component {
 
     render () {
@@ -15,13 +14,13 @@ class Blog extends Component {
                     <ul>
                         <li>
                             <NavLink 
-                                to="/" 
+                                to="/posts" 
                                 activeClassName="my-active"
                                 activeStyle={{
                                     color: '#fa923f',
                                     textDecoration: 'underline'
                                 }}
-                                exact>Home</NavLink></li>
+                                exact>Posts</NavLink></li>
                         <li><NavLink to={
                             {
                                 pathname: '/new-post',
@@ -31,12 +30,11 @@ class Blog extends Component {
                         }>New Post</NavLink></li>
                     </ul>
                 </header>
-                {/*<Route path="/" exact render={ () => <h1>Home</h1> } />
-                <Route path="/"  render={ () => <h1>Home2</h1> } />*/}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" component={FullPost} />
-            </div>
+                <Switch>
+                    <Route path="/posts" component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                </Switch>
+             </div>
         );
     }
 }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import axios from '../../../axios';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
+import FullPost from '../FullPost/FullPost';
 
 class Posts extends Component {
     state = {
@@ -31,7 +32,7 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+        this.props.history.push({ pathname: '/posts/' + id});
     }
 
     render() {
@@ -39,21 +40,24 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map((post) => {
                 return (
-                    <Link to={'/' + post.id} key={post.id}>
-                        <Post 
-                            title={post.title} 
-                            author={post.author} 
-                            clicked={() => this.postSelectedHandler(post.id)} />
-                    </Link>
+                    <Post 
+                        key={post.id}
+                        title={post.title} 
+                        author={post.author} 
+                        clicked={() => this.postSelectedHandler(post.id)} />
+                
                 );
                     
             });
         }
     
         return (
-            <section className="Posts">
-                { posts }
-            </section>
+            <div>
+                <section className="Posts">
+                    { posts }
+                </section>
+                <Route path={this.props.match.url + '/:id'} exact component={FullPost} />
+            </div>
         );
     }
 }
